ae98696bc02e02ce44f67a3b89260359e42859c9
Adarsh <adarsh.kumar02291@gmail.com>
2024-08-26 23:39:26 +0530

    changed orm from drizzle to prisma

--- src/drizzle/migrations/0002_strong_franklin_storm.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS "chat" (
-	"members" text[] DEFAULT ARRAY[]::text[] NOT NULL,
-	"timestamp1" timestamp DEFAULT now() NOT NULL
-);
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,50 +1,22 @@
 const express = require("express");
-import { db } from "../drizzle/db";
-import { UserTable } from "../drizzle/schema";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 export const userRouter = express();
 
 userRouter.use(express.json());
 
 userRouter.post("/signup", async (req: any, res: any) => {
-  const { name, email } = req.body;
-  const userExists = await db.query.UserTable.findFirst({
-    where: (UserTable, { eq }) => eq(UserTable.email, email),
+  const { username, password, firstName, lastName } = req.body;
+
+  const resp = await prisma.user.create({
+    data: {
+      username,
+      password,
+      firstName,
+      lastName,
+    },
   });
+  console.log(resp);
 
-  if (userExists) {
-    return res
-      .status(409)
-      .send({ error: "User with this email already exists." });
-  }
-  const user = await db.insert(UserTable).values({
-    name: name,
-    email: email,
-  });
-
-  res.status(201).send({ name: name });
-});
-
-userRouter.post("/signin", async (req: any, res: any) => {
-  const {  email } = req.body;
-  const userExists = await db.query.UserTable.findFirst({
-    where: (UserTable, { eq }) => eq(UserTable.email, email),
-  });
-
-  
-  if (userExists) {
-    return res.status(201).send({ name: userExists.name , email:email });
-  }
-  return res.status(409).send({ msg: `user doesnt exists ` });
-});
-
-userRouter.get("/users", async (req: any, res: any) => {
-  try {
-    const users = await db.select().from(UserTable);
-
-    // Respond with the list of users
-    res.status(200).send(users);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).send({ error: "Internal server error" });
-  }
 });
